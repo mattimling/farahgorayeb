a60554318880dc3ed504e2c5ff91f0af496e1a3d
MATT\matti <mattimling@gmail.com>
2025-05-04 19:07:20 +0200

    Fix portfolio hover

--- components/acf-blocks/_projects.php
+++ components/acf-blocks/_projects.php
@@ -128,7 +128,7 @@ $terms = get_terms( array(
 				?>
 
 				<div class="pflo-grid-item <?= $post_type == 'portfolio' ? 'pflo col-span-12 sm:col-span-6 lg:col-span-4 2xl:col-span-3' : 'swrm col-span-6 sm:col-span-4 lg:col-span-3 2xl:col-span-2'; ?> flex flex-col js-element-blurin" data-category="<?= esc_attr( $category_slugs ); ?>">
-					<a href="<?php the_permalink(); ?>" class="pflo-grid-item-inner flex flex-col gap-y-[10px] group [&.is-inactive]:grayscale [&.is-inactive]:brightness-[.85] transition-all duration-700 ease-in-out will-change-transform">
+					<a href="<?php the_permalink(); ?>" class="pflo-grid-item-inner flex flex-col gap-y-[10px] group js-pflo-item [&.is-inactive]:grayscale [&.is-inactive]:brightness-[.85] transition-all duration-700 ease-in-out will-change-transform">
 						<?php if ( has_post_thumbnail() ) : ?>
 							<div class="pflo-grid-item-img overflow-hidden relative">
 								<?= get_the_post_thumbnail( get_the_ID(), 'xl', [ 'class' => 'w-full group-hover:inverse' ] ); ?>
