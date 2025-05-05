71e158009bff24a73d9262c1477304d2628d2dd0
MATT\matti <mattimling@gmail.com>
2025-05-05 10:01:29 +0200

    Fix active menu item - take into consideration category links

--- assets/js/modules/menu.js
+++ assets/js/modules/menu.js
@@ -1,13 +1,9 @@
 function menu() {
-
     document.addEventListener('click', event => {
-
         const self = event.target;
 
         if (self.classList.contains('js-menu-open') || self.classList.contains('js-menu-close') || self.classList.contains('js-menu-link')) {
-
             event.preventDefault();
-
             const menu = document.querySelector('.js-menu');
             const menuOpen = document.querySelector('.js-menu-open');
             const menuClose = document.querySelector('.js-menu-close');
@@ -16,29 +12,24 @@ function menu() {
 
             // Open menu
             if (self.classList.contains('js-menu-open')) {
-
                 menuOpen.classList.add('is-hidden');
                 menuClose.classList.remove('is-hidden');
                 menu.classList.remove('is-close');
                 contentWrapper.classList.add('is-blurry');
                 logo.classList.add('is-inactive');
-
             }
 
             // Close menu
             if (self.classList.contains('js-menu-close')) {
-
                 menuOpen.classList.remove('is-hidden');
                 menuClose.classList.add('is-hidden');
                 menu.classList.add('is-close');
                 contentWrapper.classList.remove('is-blurry');
                 logo.classList.remove('is-inactive');
-
             }
 
             // Link clicked = close menu
             if (self.classList.contains('js-menu-link')) {
-
                 menuOpen.classList.remove('is-hidden');
                 menuClose.classList.add('is-hidden');
                 menu.classList.add('is-close');
@@ -52,13 +43,9 @@ function menu() {
                     });
                     self.classList.add('is-active');
                 }, 1000);
-
             }
-
         }
-
     });
-
 }
 
 menu();
@@ -67,19 +54,24 @@ function menuActiveItem() {
     const menuItems = document.querySelectorAll('.js-menu-link');
     const currentPath = window.location.pathname;
 
+    // Remove '/category/{slug}' part from the current path
+    const baseCurrentPath = currentPath.replace(/\/category\/[^\/]+/, '');
+
     if (menuItems) {
         menuItems.forEach(item => {
             item.classList.remove('is-active');
 
             const itemPath = new URL(item.href).pathname;
 
-            // Match exact path
-            if (itemPath === currentPath) {
+            // Remove '/category/{slug}' part from the item path
+            const baseItemPath = itemPath.replace(/\/category\/[^\/]+/, '');
+
+            // Match the base path (ignores category portion)
+            if (baseItemPath === baseCurrentPath) {
                 item.classList.add('is-active');
             }
         });
     }
 }
-
 
 menuActiveItem();
